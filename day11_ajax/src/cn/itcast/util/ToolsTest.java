523997656da17c83155a98f1fc7099ab72296b06
G_holic <G_holic@qq.com>
2023-01-12 13:11:49 +0800

    first commit

---
+++ day11_ajax/src/cn/itcast/util/ToolsTest.java
@@ -0,0 +1,8 @@
+package cn.itcast.util;
+
+public class ToolsTest {
+    public static void main(String[] args) {
+        CommonResult ok = CommonResult.ok().setResultData("信息");
+        System.out.println("ok = " + ok);
+    }
+}
