930be3bd08736de64fb6c1bcad577500227336b4
G_holic <G_holic@qq.com>
2023-01-12 13:08:20 +0800

    first commit

---
+++ day04_tomcat01/src/cn/itcast/bean/User.java
@@ -0,0 +1,7 @@
+package cn.itcast.bean;
+
+public class User {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ day04_tomcat01/src/cn/itcast/bean/demo.java
@@ -0,0 +1,12 @@
+package cn.itcast.bean;
+
+public class demo {
+    public static void main(String[] args) {
+        int i=0;
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        for (i = 0;  i<= 12; i++) {
+            arr[i] = 0;
+            System.out.println("\n"+111);
+        }
+    }
+}
